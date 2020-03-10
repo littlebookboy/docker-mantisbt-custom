cbfc31338ae56793b8af5ebc6ae8f156a6b2599d
gene <gene@js-tech.tw>
2020-03-10 21:33:42 +0800

    sh 增加變更 config 擁有者跟群組

--- initial.sh
+++ initial.sh
@@ -1,3 +1,5 @@
 chgrp -R www-data /var/www/html/codevtt
 chown -R www-data /var/www/html/codevtt
 chmod -R g+rw /var/www/html/codevtt
+chgrp -R www-data /var/www/html/config
+chown -R www-data /var/www/html/config
