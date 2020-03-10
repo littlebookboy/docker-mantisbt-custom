20abacdbcde6f9b2ae23d1fa6aef58757aa46d90
gene <gene@js-tech.tw>
2020-03-10 21:41:50 +0800

    update initial.sh

--- initial.sh
+++ initial.sh
@@ -3,3 +3,9 @@ chown -R www-data /var/www/html/codevtt
 chmod -R g+rw /var/www/html/codevtt
 chgrp -R www-data /var/www/html/config
 chown -R www-data /var/www/html/config
+chgrp www-data /var/www/html/env.php
+chown www-data /var/www/html/env.php
+chgrp www-data /var/www/html/autoload.php
+chown www-data /var/www/html/autoload.php
+chgrp -R www-data /var/www/html/plugins
+chown -R www-data /var/www/html/plugins
